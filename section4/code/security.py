c9d1e1bc7d965d20956e9c863148f967549aea7f
Wei Wan <wei.wan@uni3t.com>
2018-08-06 12:01:41 -0400

    Flask RESTful section4

---
+++ section4/code/security.py
@@ -0,0 +1,20 @@
+#from workzeug.security import safe_str_ccmp       # for Python 2.7 it may need to compare strings.
+from user import User
+
+users = [
+    User(1, 'bob','asdf')
+]
+
+username_mapping = { u.username: u for u in users}
+
+userid_mapping = {u.id: u for u in users}
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+    #if user and safe_str_ccmp(user.password, password):
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mapping.get(user_id, None)
